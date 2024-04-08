e18e4959ca2b9830263fc4a172b2fbb08741e694
Alex Yeung (alyeung) <alyeung@cisco.com>
2024-04-07 21:07:51 -0700

    decrease click

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var numOfClicks = 0;
 
 $(document).ready(function() {
   $("#click-button").on("click", function() {
-    numOfClicks += 2;
+    numOfClicks += 1;
     document.getElementById("click-counter").innerHTML = numOfClicks;
   }); 
 });
